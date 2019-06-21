037cb3ddd68fd881bd5b146dedce75d5bb534087
Jordan Woloschuk <jordan.woloschuk@gmail.com>
2019-06-21 14:36:48 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -76,7 +76,7 @@ selected_DF <- select(total_DF, Subject, Activity_ID, contains("mean"), contains
 # Change the Acticity ID to the descriptive activity type
 selected_DF$Activity_ID <- activites_labels[selected_DF$Activity_ID,2]
 
-# Relable column headers to be more descriptive
+# Relabel column headers to be more descriptive
 
 names(selected_DF) <- gsub("^t", "Time",names(selected_DF))
 names(selected_DF) <- gsub("^f", "Frequency", names(selected_DF))
